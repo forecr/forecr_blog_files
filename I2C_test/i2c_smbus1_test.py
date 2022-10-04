12f9dbabe668060508cc671703efbd15ec9d6ae9
mdeniz0 <mehmetdeniz@mistelektronik.com>
2022-10-04 14:27:52 +0300

    I2C test Python source codes added

---
+++ I2C_test/i2c_smbus1_test.py
@@ -0,0 +1,12 @@
+from smbus import SMBus
+import time
+
+def main():
+	i2cbus = SMBus(1)
+	i2caddress = 0x4C
+	a=i2cbus.read_byte_data(i2caddress, 0x05)
+	print(a)
+	time.sleep(1)
+if __name__ == "__main__":
+    main()
+
